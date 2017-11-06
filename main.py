a3afe08754e801b9ead519bad4b7ff430f0a8214
Bryant Hayes <Bryant.Hayes@cognex.com>
2017-11-06 13:13:10 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: bhayes
+# @Date:   2017-11-06 13:08:59
+# @Last Modified by:   bhayes
+# @Last Modified time: 2017-11-06 13:09:58
+import tdl
+
+def main():
+	print("Launching ProjectRL...")
+
+if __name__ == "__main__":
+	main()
