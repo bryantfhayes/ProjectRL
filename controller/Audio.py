f542d2fc296bfe76c707b7192e21e6dd5fe8d3c4
Bryant Hayes <bryantfhayes@gmail.com>
2017-11-07 01:09:24 -0800

    Added Audio support

--- controller/Audio.py
+++ controller/Audio.py
@@ -5,16 +5,19 @@
 # @Last Modified time: 2017-11-06 15:24:02
 from model.Message import Message, MsgType
 from controller.System import System
+from playsound import playsound
 
 class Audio(System):
 	def __init__(self, msgBus):
 		System.__init__(self, msgBus)
+		self.subscriptions.extend([MsgType.eMsgType_RemoveEntity])
 
 	def init(self):
 		System.init(self)
 
 	def handleMessage(self, msg):
-		pass
+		if msg.msgType == MsgType.eMsgType_RemoveEntity:
+			playsound('assets/click.wav', block=False)
 
 	def update(self):
 		pass
